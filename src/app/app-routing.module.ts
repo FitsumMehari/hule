a6ceb881f61e379684e4c45e56f7edf52b347c09
Itisme <Eyobamane09@gmail.com>
2022-06-18 17:11:10 +0300

    1) Fix the Navigation Issue; 2) Replace the home-body-component by home-page-components;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogsComponent } from './pages/blogs/blogs.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 const routes: Routes = [
-  { path: 'blog', component: BlogsComponent }
+  { path: 'blog', component: BlogsComponent },
+  { path: 'home', component: HomePageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { IntroBackComponent } from './components/intro-back/intro-back.component';
-import { HomeBodyComponent } from './components/home-body/home-body.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BlogsComponent } from './pages/blogs/blogs.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     IntroBackComponent,
-    HomeBodyComponent,
     FooterComponent,
-    BlogsComponent
+    BlogsComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,

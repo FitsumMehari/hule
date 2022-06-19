4c64d88c6526729c6ea13f956e6dffffe330b9e7
Itisme <Eyobamane09@gmail.com>
2022-06-19 14:50:38 +0300

    Actiavte Modal when user click add to cart in store page ..... and fix minor bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { BlogsComponent } from './pages/blogs/blogs.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { StorePageComponent } from './pages/store-page/store-page.component';
-
+import { ModalComponent } from './components/modal/modal.component';
 
 const routes: Routes = [
   { path: 'blog', component: BlogsComponent },
   { path: 'home', component: HomePageComponent },
-  { path:  'store', component: StorePageComponent}
+  { path:  'store', component: StorePageComponent},
+  { path: 'modal', component: ModalComponent }
 ];
 
 @NgModule({
--- src/app/pages/store-page/store-page.component.ts
+++ src/app/pages/store-page/store-page.component.ts
@@ -8,9 +8,22 @@ declare function sayChees(): void;
   styleUrls: ['./store-page.component.css']
 })
 export class StorePageComponent implements OnInit {
+  public mActiveSideBar: boolean[] = [false, false, false, false, false, false, false];
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
+  setActiveTab(index:number):void 
+  {
+    for(let i = 0; i < 7; i++) 
+    {
+      if(i == index) this.mActiveSideBar[i] = true;
+      else this.mActiveSideBar[i] = false;
+    }
+  }
+
 
 }

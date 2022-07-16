454c1aa1d2c2f90344c5f3b2085791d2de03f0ae
Fitsum Mehari <m.fitsummehari@gmail.com>
2022-07-16 13:47:15 +0300

    added cart, order and product models

---
+++ api/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    products: [
+        {
+            productId: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'Pending'
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('order', OrderSchema);
---
+++ api/models/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String
+    },
+    price: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('product', ProductSchema);
